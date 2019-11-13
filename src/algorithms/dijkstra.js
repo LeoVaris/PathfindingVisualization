27c2c730709418034218ddfc50b5091e8724d741
Leo Varis <49756722+LeoVaris@users.noreply.github.com>
2019-11-13 19:25:18 +0200

    Update dijkstra.js

--- src/algorithms/dijkstra.js
+++ src/algorithms/dijkstra.js
@@ -1,31 +1,30 @@
-
 import {neighbors, GridToNodeArray, sortNodesByDistance} 
 from './generalAlgorithms';
 
 
 export function dijkstra(grid, start_node, end_node) {
   const visitedNodes = [];
-  start_node.distance = 0;
-  const unVisitedNodes = GridToNodeArray(grid);
-  while (unVisitedNodes.length) {
-    sortNodesByDistance(unVisitedNodes);
-    const closestNode = unVisitedNodes.shift();
+  start_node.distance = 0; // start with 0 dist
+  const unVisitedNodes = GridToNodeArray(grid); // all are unvisited
+  while (unVisitedNodes.length) { // while theres nodes to visit
+    sortNodesByDistance(unVisitedNodes); // sort nodes
+    const closestNode = unVisitedNodes.shift(); // removes the first item
 
-    if (closestNode.isWall) continue;
+    if (closestNode.isWall) continue; // if its wall ignore it
 
-    if (closestNode.distance === Infinity) return visitedNodes;
-    closestNode.isVisited = true;
-    visitedNodes.push(closestNode);
-    if (closestNode.row === end_node.row && closestNode.col === end_node.col) 
+    if (closestNode.distance === Infinity) return visitedNodes; // theres no path
+    closestNode.isVisited = true; // "visit" this node
+    visitedNodes.push(closestNode); 
+    if (closestNode.row === end_node.row && closestNode.col === end_node.col)  // if this is end
       return visitedNodes;
-    updateNeighbors(closestNode, grid);
+    updateNeighbors(closestNode, grid); // update this nodes neighbors
   }
 }
 
 function updateNeighbors(node, grid) {
   const nb = neighbors(grid, node, grid.length, grid[0].length);
   for (const neighbor of nb) {
-    neighbor.distance = node.distance + 1;
+    neighbor.distance = node.distance + 1; // distance is 1 more than where it came from
     neighbor.previousNode = node;
   }
 }
